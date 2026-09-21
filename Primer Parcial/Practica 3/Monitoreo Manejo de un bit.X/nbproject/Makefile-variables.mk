5d1e51df2b232e885ca1fb5660173a766f265738
Quirino <quirino.gonzalez.johann.david@gmail.com>
2026-09-20 21:45:27 -0600

    Fin de practicas del primer parcia

---
+++ Primer Parcial/Practica 3/Monitoreo Manejo de un bit.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=Monitoreo_Manejo_de_un_bit.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/Monitoreo_Manejo_de_un_bit.X.production.hex
