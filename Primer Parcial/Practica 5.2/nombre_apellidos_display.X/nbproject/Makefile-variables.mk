a4f57741b3b1683c380653a9ca89f69435325fcb
Quirino <quirino.gonzalez.johann.david@gmail.com>
2026-09-25 22:48:12 -0600

    Faltan cosas

---
+++ Primer Parcial/Practica 5.2/nombre_apellidos_display.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=nombre_apellidos_display.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/nombre_apellidos_display.X.production.hex
